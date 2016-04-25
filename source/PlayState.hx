d7bff96b1142e7fad1a0cba9f99d908e5eb1fbda
saumya <2saumyaray@gmail.com>
2016-04-26 01:36:49 +0530

    added square

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -10,13 +10,17 @@ import flixel.math.FlxMath;
 class PlayState extends FlxState
 {
 	private var t:FlxText = null;
+	private var _player:Player;
 
 	override public function create():Void
 	{
-		super.create();
-		//
 		this.t= new FlxText(0,0,0,"Welcome to Play State");
 		this.add(this.t);
+		//
+		_player = new Player(20, 20);
+ 		add(_player);
+ 		// final call
+ 		super.create();
 	}
 
 	override public function update(elapsed:Float):Void
