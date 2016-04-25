12ff3142a3f66887ba95e98bca1aaae6e92fef39
saumya <2saumyaray@gmail.com>
2016-04-26 00:24:11 +0530

    init

---
+++ source/MenuState.hx
@@ -0,0 +1,26 @@
+package;
+
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.FlxState;
+import flixel.text.FlxText;
+import flixel.ui.FlxButton;
+import flixel.math.FlxMath;
+
+class MenuState extends FlxState
+{
+	var t:FlxText = null;
+
+	override public function create():Void
+	{
+		super.create();
+		//
+		this.t= new FlxText(0,0,0,"Hello");
+		this.add(this.t);
+	}
+
+	override public function update(elapsed:Float):Void
+	{
+		super.update(elapsed);
+	}
+}
