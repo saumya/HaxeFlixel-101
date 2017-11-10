afc4885f63059b5f66b78bf730a00df86ee13ea3
saumya <2saumyaray@gmail.com>
2017-11-10 17:43:50 +0530

    learning flixel

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -7,6 +7,9 @@ import flixel.text.FlxText;
 import flixel.ui.FlxButton;
 import flixel.math.FlxMath;
 
+import flixel.util.FlxColor;
+import flixel.util.FlxSpriteUtil;
+
 class PlayState extends FlxState
 {
 	private var t:FlxText = null;
@@ -16,13 +19,22 @@ class PlayState extends FlxState
 	{
 		this.t= new FlxText(0,0,0,"Welcome to Play State");
 		this.add(this.t);
-		//
+		
+ 		//
+ 		var _sprBack = new FlxSprite().makeGraphic(200, 200, FlxColor.BLACK);
+        //FlxSpriteUtil.drawRect(_sprBack, 0, 0, FlxG.width, FlxG.height, FlxColor.WHITE);
+        FlxSpriteUtil.drawRect(_sprBack, 0, 0, 100, 100, FlxColor.WHITE);
+        add(_sprBack);
+
+        //
 		_player = new Player(20, 20);
  		add(_player);
 
  		//Camera follow the actor
  		FlxG.camera.follow(_player, TOPDOWN, 1);
- 		
+
+
+
  		// final call
  		super.create();
 	}
